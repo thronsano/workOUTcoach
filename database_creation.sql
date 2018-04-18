09b170ad44014fb8eaf653218c4eae6cb2139434
Piotr Gonera <piotrgonera95@gmail.com>
2018-04-18 12:09:48 +0200

    Updated SQL needed to create database, added sample clients

--- database_creation.sql
+++ database_creation.sql
@@ -4,10 +4,13 @@ CREATE TABLE clients (
   email VARCHAR(100)
 );
 
+INSERT INTO clients (id, name, email) VALUES (1, 'Steve Stevinsky', 'superSteve@gmail.com');
+INSERT INTO clients (id, name, email) VALUES (2, 'Kate Rabbit', 'katyy@gmail.com');
+
 CREATE TABLE users (
   id INT PRIMARY KEY AUTO_INCREMENT,
   username VARCHAR(20) UNIQUE NOT NULL,
-  password VARCHAR(30) NOT NULL,
+  password VARCHAR(100) NOT NULL,
   name VARCHAR(50) NOT NULL,
   surname VARCHAR(50) NOT NULL,
   securityQuestion VARCHAR(255) NOT NULL,
@@ -16,7 +19,7 @@ CREATE TABLE users (
 );
 
 -- Sample user's password '$2a$10$9d5AC2CrUGaWSgwRHbtZV.TbKiuixWQh3EzJhZ7tHt0AeifE2AxCq' is a hashed version of password 'password'
-INSERT into users(username, password, name, surname, securityQuestion, securityAnswer, email) VALUES ('User1', '$2a$10$9d5AC2CrUGaWSgwRHbtZV.TbKiuixWQh3EzJhZ7tHt0AeifE2AxCq', 'Steve', 'Doe', 'Whats ur name', 'Steve', 'sdoe@gmail.com');
+INSERT INTO users(username, password, name, surname, securityQuestion, securityAnswer, email) VALUES ('User1', '$2a$10$9d5AC2CrUGaWSgwRHbtZV.TbKiuixWQh3EzJhZ7tHt0AeifE2AxCq', 'Steve', 'Doe', 'Whats ur name', 'Steve', 'sdoe@gmail.com');
 
 create table authorities (
   userId INT NOT NULL,
