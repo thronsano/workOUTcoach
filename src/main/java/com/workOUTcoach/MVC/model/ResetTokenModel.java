4906364489f1fca2ba85a99523d2cacaf04e3644
Piotr Gonera <piotrgonera95@gmail.com>
2018-05-20 15:26:03 +0200

    fixed another occurrence of the openSession freeze bug

--- src/main/java/com/workOUTcoach/MVC/model/ResetTokenModel.java
+++ src/main/java/com/workOUTcoach/MVC/model/ResetTokenModel.java
@@ -5,6 +5,7 @@ import com.workOUTcoach.entity.ResetToken;
 import com.workOUTcoach.utility.Logger;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -15,39 +16,44 @@ public class ResetTokenModel {
     private SessionFactory sessionFactory;
 
     public ResetToken getByResetToken(String resetToken) {
-        return (ResetToken) sessionFactory.openSession().createQuery("from ResetToken rt where rt.resetToken = '"+resetToken+"'").uniqueResult();
-    }
+        Session session = sessionFactory.openSession();
 
-    public ResetToken getResetTokenByEmail(String email) {
-        //return (ResetToken) sessionFactory.openSession().createQuery("from ResetToken rt where rt.email = '"+email+"'").getResultList().get(0);
-        return sessionFactory.openSession().get(ResetToken.class, email);
-    }
-
-    public boolean saveResetToken(ResetToken resetToken) {
         try {
-            Session session = sessionFactory.openSession();
             session.beginTransaction();
-            ResetToken updateResetToken = session.get(ResetToken.class, resetToken.getEmail());
-            updateResetToken.setResetToken(resetToken.getResetToken());
+
+            Query query = session.createQuery("from ResetToken rt where rt.resetToken =:resetToken");
+            query.setParameter("resetToken", resetToken);
+
+            return (ResetToken) query.uniqueResult();
+        } finally {
             session.getTransaction().commit();
             session.close();
-        } catch (Exception e) {
-            Logger.logError("Exception during saving ResetToken into database");
-            return false;
         }
-        return true;
+    }
+
+    ResetToken getResetTokenByEmail(String email) {
+        Session session = sessionFactory.openSession();
+
+        try {
+            session.beginTransaction();
+            return sessionFactory.openSession().get(ResetToken.class, email);
+        } finally {
+            session.getTransaction().commit();
+            session.close();
+        }
     }
 
     public boolean addResetToken(ResetToken resetToken) {
-
         deleteResetToken(resetToken.getEmail());
+
         try {
             Session session = sessionFactory.openSession();
             session.beginTransaction();
+
             session.save(resetToken);
+
             session.getTransaction().commit();
             session.close();
-
         } catch (Exception e) {
             Logger.logError("Exception during adding new resetToken into database");
             return false;
@@ -57,20 +63,23 @@ public class ResetTokenModel {
 
     public boolean deleteResetToken(String email) {
         ResetToken existingToken = getResetTokenByEmail(email);
-        if(existingToken!=null){
+
+        if (existingToken != null) {
             try {
                 Session session = sessionFactory.openSession();
                 session.beginTransaction();
+
                 session.delete(existingToken);
+
                 session.getTransaction().commit();
                 session.close();
-            }catch (Exception e){
+            } catch (Exception e) {
                 Logger.logError("Exception during removing existing resetToken");
                 return false;
             }
-        }else {
+        } else
             Logger.log("Reset token doesn't exist");
-        }
+
         return true;
     }
 }
