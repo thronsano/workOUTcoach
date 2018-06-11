e6ca3d46631d33e2fbbcf20bb083dd94c97f1dc9
Piotr Gonera <piotrgonera95@gmail.com>
2018-06-12 00:29:16 +0200

    different progress values to make it look cooler

--- database_creation.sql
+++ database_creation.sql
@@ -104,8 +104,8 @@ INSERT INTO authorities (email, authority) VALUES ('wiktoria.malawska@wp.pl', 'R
 INSERT INTO authorities (email, authority) VALUES ('tomaszewski-eryk@wp.pl', 'ROLE_USER');
 INSERT INTO authorities (email, authority) VALUES ('wiktoria.malawska@test.pl', 'ROLE_USER');
 
-INSERT INTO clients (name, surname, coachEmail, gymName, goal, goalValue, healthCondition, isActive) VALUES ('Steve', 'Stevinsky', 'sdoe@gmail.com', 'Jatomi', 'Muscle gain', 0, 'Healthy', true);
-INSERT INTO clients (name, surname, coachEmail, gymName, goal, goalValue, healthCondition, isActive) VALUES ('Kate', 'Rabbit', 'sdoe@gmail.com', 'Platinium', 'To be fit!', 0, 'healthy', true);
+INSERT INTO clients (name, surname, coachEmail, gymName, goal, goalValue, healthCondition, isActive) VALUES ('Steve', 'Stevinsky', 'sdoe@gmail.com', 'Jatomi', 'Muscle gain', 23, 'Healthy', true);
+INSERT INTO clients (name, surname, coachEmail, gymName, goal, goalValue, healthCondition, isActive) VALUES ('Kate', 'Rabbit', 'sdoe@gmail.com', 'Platinium', 'To be fit!', 87, 'healthy', true);
 INSERT INTO clients (name, surname, coachEmail, goalValue, isActive) VALUES ('Paul', 'Old', 'sdoe@gmail.com', '0', false);
 INSERT INTO clients (name, surname, coachEmail, goalValue, isActive) VALUES ('William', 'McDonald', 'wiktoria.malawska@wp.pl', '0', true);
 INSERT INTO clients (name, surname, coachEmail, goalValue, isActive) VALUES ('Ann', 'McDonald2', 'wiktoria.malawska@wp.pl', '0', true);
