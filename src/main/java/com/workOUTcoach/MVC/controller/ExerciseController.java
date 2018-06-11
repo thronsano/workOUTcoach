48e2f7d1cba35af1ba034850314fb11101c8ee65
Eryk Tomaszewski <tomaszewski-eryk@wp.pl>
2018-06-11 21:36:31 +0200

    creating new exercises

--- src/main/java/com/workOUTcoach/MVC/controller/ExerciseController.java
+++ src/main/java/com/workOUTcoach/MVC/controller/ExerciseController.java
@@ -1,6 +1,7 @@
 package com.workOUTcoach.MVC.controller;
 
 import com.workOUTcoach.MVC.model.ExerciseModel;
+import com.workOUTcoach.MVC.model.SchemeModel;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping(value = "clientProfile/training/scheme")
@@ -19,11 +21,27 @@ public class ExerciseController {
     @Autowired
     ExerciseModel exerciseModel;
 
+    @Autowired
+    SchemeModel schemeModel;
+
     @RequestMapping(value = "/newExercise", method = RequestMethod.GET)
-    public ModelAndView editTitle(ModelAndView modelAndView,
-                                  @RequestParam(value = "schemeId") int schemeID){
+    public ModelAndView getNewExercise(ModelAndView modelAndView,
+                                       @RequestParam(value = "schemeId") int schemeID) {
+        modelAndView.addObject("scheme", schemeModel.getSchemeById(schemeID));
         modelAndView.setViewName("newExercise");
         return modelAndView;
     }
 
+
+    @RequestMapping(value = "/newExercise", method = RequestMethod.POST)
+    public ModelAndView postNewExercise(ModelAndView modelAndView,
+                                        @RequestParam(value = "schemeID") int schemeID,
+                                        @RequestParam(value = "name", required = false, defaultValue = "") String name,
+                                        @RequestParam(value = "repetitions", required = false, defaultValue = "0") int repetitions,
+                                        RedirectAttributes redirectAttributes) {
+        exerciseModel.addNewExercise(schemeID, name, repetitions);
+        redirectAttributes.addFlashAttribute("scheme", schemeModel.getSchemeById(schemeID));
+        modelAndView.setViewName("redirect:/clientProfile/training/scheme?schemeId=" + schemeID);
+        return modelAndView;
+    }
 }
